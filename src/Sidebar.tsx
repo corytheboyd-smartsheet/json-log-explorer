20ba05c48da0032c6f70acde32941ebfb49167e8
Cory Boyd <cory.boyd@smartsheet.com>
2022-09-22 17:51:16 -0600

    Buttons overflow

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -25,14 +25,20 @@ export const Sidebar: React.FC = () => {
   }, [setSidebarCollapsed]);
 
   return (
-    <div className="h-screen flex flex-col space-y-3 bg-gray-100 shadow">
+    <div className="h-screen flex flex-col space-y-3 bg-gray-100 shadow overflow-scroll pb-10">
       {sidebarCollapsed && (
-        <div className="bg-gray-500 h-screen p-1">
+        <div className="bg-gray-500 h-screen p-1 flex flex-col space-y-2">
           <button
             className="bg-amber-500 rounded p-1 h-24"
             onClick={handleShowSidebar}
           >
             👉
+          </button>
+          <button
+            className="bg-red-400 rounded p-1 h-24"
+            onClick={handleClearLogs}
+          >
+            🔥
           </button>
         </div>
       )}
@@ -46,7 +52,7 @@ export const Sidebar: React.FC = () => {
                 className="bg-red-500 rounded text-white"
                 onClick={handleClearLogs}
               >
-                Delete Logs
+                Delete Logs 🔥
               </button>
               <button
                 className="bg-blue-500 rounded text-white"
@@ -58,7 +64,7 @@ export const Sidebar: React.FC = () => {
                 className="bg-amber-500 rounded text-white"
                 onClick={handleCollapseSidebar}
               >
-                🤏
+                Collapse 🤏
               </button>
             </div>
           </div>
