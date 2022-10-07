8cdcdf1587b5d2abf666e39e5bf565c47dd171ed
Cory Boyd <cory.boyd@smartsheet.com>
2022-10-07 11:10:16 -0600

    Save connections

--- src/lib/store/useStore.ts
+++ src/lib/store/useStore.ts
@@ -1,8 +1,233 @@
 import create from "zustand";
 import { persist } from "zustand/middleware";
-import { buildStateCreator } from "./buildStateCreator";
-import { buildPersistMiddleware } from "./buildPersistMiddleware";
+import { createMapFromObject } from "../createMapFromObject";
+import { getPaths } from "../getPaths";
+import { Socket } from "../Socket";
+import { getFromMapAtPath } from "../getFromMapAtPath";
+import { v4 } from "uuid";
+import { AppStore, AppStoreData } from "./types";
 
-export const useStore = create(
-  persist(buildStateCreator(), buildPersistMiddleware())
+export const useStore = create<AppStore>()(
+  persist(
+    (set, get) => ({
+      logs: [],
+      paths: new Set<string>(),
+      selectedPaths: new Set<string>(),
+      selectedLog: null,
+      sidebarCollapsed: false,
+      pathSearchQuery: "",
+      sectionCollapsed: {},
+      connections: {},
+      filters: {},
+      addLog: (raw) => {
+        const { logs, paths } = get();
+
+        const map = createMapFromObject(raw);
+
+        const newPaths = new Set(paths);
+        getPaths(map).forEach((path) => newPaths.add(path));
+
+        const log = {
+          raw: raw,
+          data: map,
+        };
+
+        set({
+          paths: newPaths,
+          logs: [log, ...logs],
+        });
+      },
+      addSelectedPath: (path) => {
+        const { selectedPaths } = get();
+
+        const newPaths = new Set(Array.from(selectedPaths));
+        newPaths.add(path);
+
+        set({ selectedPaths: newPaths });
+      },
+      removeSelectedPath: (path) => {
+        const { selectedPaths } = get();
+
+        const newPaths = new Set(Array.from(selectedPaths));
+        newPaths.delete(path);
+
+        set({ selectedPaths: newPaths });
+      },
+      clearSelectedPaths: () => {
+        set({ selectedPaths: new Set() });
+      },
+      clearLogs: () => {
+        set({ logs: [], selectedLog: null });
+      },
+      setSelectedLog: (log) => set({ selectedLog: log }),
+      clearSelectedLog: () => set({ selectedLog: null }),
+      setSidebarCollapsed: (value) => set({ sidebarCollapsed: value }),
+      setPathSearchQuery: (query) => set({ pathSearchQuery: query }),
+      addConnection: (address) => {
+        const connections = get().connections;
+        if (connections[address]) {
+          return;
+        }
+
+        connections[address] = {
+          status: "initial",
+          socket: new Socket(address),
+        };
+        set({ connections });
+      },
+      removeConnection: (address) => {
+        const connections = get().connections;
+        delete connections[address];
+        set({ connections });
+      },
+      updateConnection: (address, changes) => {
+        const connections = get().connections;
+        const current = connections[address];
+        connections[address] = { ...current, ...changes };
+        set({ connections });
+      },
+      resetConnection: (address) => {
+        const connections = get().connections;
+        const connection = connections[address];
+        if (!connection) {
+          return;
+        }
+
+        get().updateConnection(address, { status: "initial" });
+        connection.socket.reset();
+      },
+      setSectionCollapsed: (name, value) => {
+        const sectionCollapsed = get().sectionCollapsed;
+        sectionCollapsed[name] = value;
+        set({ sectionCollapsed });
+      },
+      getFilteredLogs: () => {
+        const { logs, ...state } = get();
+        const filters = Object.values(state.filters);
+
+        return logs.filter((log) => {
+          return filters.every((filter) => {
+            const valueAtPath = getFromMapAtPath(log.data, filter.path);
+
+            switch (filter.strategy) {
+              case "contains": {
+                return valueAtPath.includes(filter.value);
+              }
+              case "excludes": {
+                return !valueAtPath.includes(filter.value);
+              }
+              case "eq": {
+                return valueAtPath === filter.value;
+              }
+              case "neq": {
+                return valueAtPath !== filter.value;
+              }
+              case "gt": {
+                return valueAtPath > filter.value;
+              }
+              case "gte": {
+                return valueAtPath >= filter.value;
+              }
+              case "lt": {
+                return valueAtPath < filter.value;
+              }
+              case "lte": {
+                return valueAtPath <= filter.value;
+              }
+              default: {
+                return true;
+              }
+            }
+          });
+        });
+      },
+      addFilter: (filter) => {
+        const filters = get().filters;
+        const newFilter = { id: v4(), ...filter };
+        filters[newFilter.id] = newFilter;
+        set({ filters });
+      },
+      removeFilter: (id) => {
+        const filters = get().filters;
+        delete filters[id];
+        set({ filters });
+      },
+    }),
+
+    {
+      name: "json-log-explorer",
+      serialize: (storageValue) => {
+        const normalizedState: Partial<Record<keyof AppStoreData, any>> = {
+          connections: [],
+        };
+
+        Object.keys(storageValue.state).forEach((key) => {
+          // if (key === "logs") {
+          //   normalizedState[key] = storageValue.state.logs.map(
+          //     (log) => log.raw
+          //   );
+          // }
+
+          if (key === "connections") {
+            Object.values(storageValue.state.connections).forEach(
+              (connection) => {
+                normalizedState.connections.push(connection.socket.address);
+              }
+            );
+          }
+        });
+
+        const serialized = JSON.stringify({ state: normalizedState });
+        console.log("serialized", serialized);
+        return serialized;
+      },
+      deserialize: (storageValueString) => {
+        const storageValue: {
+          state: Partial<Record<keyof AppStoreData, any>>;
+          version?: number;
+        } = JSON.parse(storageValueString);
+
+        const hydratedState: Pick<AppStoreData, "logs" | "connections"> = {
+          logs: [],
+          connections: {},
+        };
+
+        console.log("RAW DESERIALIZED", storageValue);
+
+        Object.keys(storageValue.state).forEach((key) => {
+          // if (key === "logs") {
+          //   const logsData = normalizedState.logs as Log["raw"][];
+          //   logsData.forEach((logData) => {
+          //     hydratedState.logs.push({
+          //       data: createMapFromObject(logData),
+          //       raw: logData,
+          //     });
+          //   });
+          // }
+
+          if (key === "connections") {
+            if (!(storageValue.state.connections instanceof Array)) {
+              console.log("CONNECTIONS EMPTY OR MISSING");
+              return;
+            }
+            console.log("HAVE CONNECTIONS", storageValue.state.connections);
+            storageValue.state.connections.forEach((address) => {
+              console.log("HYDRATE CONNECTION", address);
+
+              hydratedState.connections[address] = {
+                status: "initial",
+                socket: new Socket(address),
+              };
+            });
+          }
+        });
+
+        const deserialized = {
+          state: hydratedState,
+        };
+        console.log("DESERIALIZED", deserialized);
+        return deserialized;
+      },
+    }
+  )
 );
